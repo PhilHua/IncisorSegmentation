3421041b9f07dcfc107333396f70d5560da72920
Sebastijan <sebastijan.dumancic@gmail.com>
2014-04-15 18:27:13 +0200

    cleaning utils

--- src/experiments.py
+++ src/experiments.py
@@ -4,15 +4,9 @@ __author__ = 'Sebastijan'
 import DataManipulations
 import ActiveShapeModel
 
-tmpObj = DataManipulations.DataCollector('../data/Landmarks/original/landmarks7-4.txt')
-DataManipulations.Plotter.render_landmarks(tmpObj)
-
 res = DataManipulations.collect_vectors('../data/Landmarks/original', '4', 80)
 
 referent = ActiveShapeModel.ReferentModel(res)
 referent.align()
 
-model = referent.retrieve_mean_model()
-model.rescale()
-model.realign_to_absolute()
-DataManipulations.Plotter.render_landmarks(model)
+print referent.retrieve_as_matrix()
